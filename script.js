18bec66870b7528d42d79a0ca7fc38a12fc8f627
Trey Krause <tjkrause49@gmail.com>
2023-11-18 17:02:00 -0500

    Add functionality to new book button
    Fixed displayBooks function so it resets after each change.
    
    Added transition to popup menu.
    
    Added event listeners to close and submit buttons on popup.
    
    Added basic styling to book cards and library.

--- script.js
+++ script.js
@@ -8,42 +8,86 @@ function Book(title, author, pages, read) {
     this.read = read
     this.info = title + ' by ' + author + ', ' + pages + ' pages, ' + read
 }
-
-const dune = new Book('Dune', 'Frank Herbert', 593, 'read');
-myLibrary.push(dune);
-
-
-/*Write a function that loops through the array and 
-displays each book on the page. 
-
-You can display them in some sort of table, 
-or each on their own “card”. 
-
-It might help for now to manually add a few books to 
-your array so you can see the display.
-*/
  
 const container = document.querySelector('#container')
 
 function displayBooks() {
+    container.textContent = '';
     myLibrary.forEach((el) => {
         const card = document.createElement('div');
-        card.classList.add('card')
+        card.classList.add('card');
         card.appendChild(document.createElement('h1'));
+        card.appendChild(document.createElement('button'));
         card.appendChild(document.createElement('h2'));
         card.appendChild(document.createElement('p'));
-        card.appendChild(document.createElement('div'))
+        card.appendChild(document.createElement('div'));
         const h1 = card.querySelector('h1');
         const h2 = card.querySelector('h2');
         const p = card.querySelector('p');
         const div = card.querySelector('div');
+        const button = card.querySelector('button');
         h1.textContent = el.title;
         h2.textContent = el.author;
         p.textContent = el.pages;
         div.classList.add(el.read);
-        container.appendChild(card);
-        console.log(card)
-    })
-}
+        div.textContent = 'O';
+        button.textContent = 'X';
 
-displayBooks();
+        button.addEventListener('click', () => {
+            container.removeChild(card);
+            myLibrary.splice(myLibrary.indexOf(card) - 1, 1);
+        });
+
+        div.addEventListener('click', () => {
+            if (div.classList.contains('true')) {
+                div.classList.remove('true');
+            } else {
+                div.classList.add('true');
+            }
+        });
+
+        container.appendChild(card);
+    })
+};
+
+const openPopupBtn = document.querySelector('#add-book');
+const popup = document.querySelector('#popup');
+const closeBtn = document.querySelector('.close-button');
+const overlay = document.querySelector('#overlay');
+const submitBtn = document.querySelector('#submit-button');
+
+openPopupBtn.addEventListener('click', () => {
+    popup.classList.add('active');
+    overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    popup.classList.remove('active');
+    overlay.classList.remove('active');
+});
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const readStatus = document.querySelector('#read');
+
+    const newBook = new Book(title.value, author.value, pages.value, readStatus.checked);
+    myLibrary.push(newBook);
+    displayBooks();
+    popup.classList.remove('active');
+    overlay.classList.remove('active');
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+});
+
+
+
